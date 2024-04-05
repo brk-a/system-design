732e05cb3e3fd6f1454acabae04de8467f726ec8
brk-a <fnjakai@outlook.com>
2024-04-05 08:14:37 +0300

    Lines

---
+++ 0x00-system_design/0-livestreaming_app.java
@@ -0,0 +1,14 @@
+public class VideoConsumingService{}
+
+public class Video{
+    String Id;
+    Frame[] frames;
+    Object jsonMetadata;
+}
+
+public class Frame{
+    byte[] bytes;
+    DateTime timestamp;
+}
+
+public class User{}
